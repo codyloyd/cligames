8d20cdc287bab57b6e0066fdf593f15361057b6b
Cody Loyd <codyloyd@gmail.com>
2016-06-27 11:21:38 -0500

    done for the moment.. still some work to be done

--- tic_tac_toe/refactor1/game.rb
+++ tic_tac_toe/refactor1/game.rb
@@ -1,12 +1,11 @@
 require "./player"
 
 class Game
-	attr_accessor :player_1, :player_2, :game_over, :game_array
+	attr_accessor :player_1, :player_2, :game_array
 
 	def initialize
 		empty_board
 		@solution_array = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-		@game_over = false
 	end
 
 	def empty_board
@@ -39,46 +38,37 @@ class Game
 		puts
 		puts "#{@player_1.name} is #{@player_1.marker}, #{@player_2.name} is #{@player_2.marker}"
 		puts
-		print_board
 	end
-
-	# def input(player)
-	# 	puts "#{player.name}, please enter your play."
-	# 	response = gets.chomp
-	# 	check_response(response,player)
-	# end
 
 	def check_response(response,player)
 		if response.to_i.between?(1,9)
 			if game_array[response.to_i-1] == " "
 				enter_play(player,response)
+				return nil
 			else
-				puts "That space is taken, try again"
-				input(player)
+				return "That space is taken, try again"
 			end
 		else
-			puts "invalid response (try a number, 1-9)"
-			input(player)
+			return "invalid response (try a number, 1-9)"
 		end
 	end
 
 	def enter_play(player,square)
 			game_array[square.to_i-1] = player.marker
-			print_board
-			game_over?
 	end
 
 	def game_over?
 		if game_array.select.include?(" ")
 			@solution_array.each do |sol|
 				if game_array[sol[0]] == "X" && game_array[sol[1]] == "X" && game_array[sol[2]] == "X"
-					@game_over = true
+					return true
 				elsif game_array[sol[0]] == "O" && game_array[sol[1]] == "O" && game_array[sol[2]] == "O"
-					@game_over = true
+					return true
 				end
 			end
 		else
-			@game_over = true
+			return true
 		end
+		return false
 	end
 end
--- tic_tac_toe/refactor1/play_game.rb
+++ tic_tac_toe/refactor1/play_game.rb
@@ -1,23 +1,29 @@
 require "./game.rb"
 game = Game.new
-
 puts "Player 1, please enter your name"
 p1 = gets.chomp
-puts "Player 1, please enter your name"
+puts "Player 2, please enter your name"
 p2 = gets.chomp
 game.create_players(p1,p2)
 game.print_help
 
 player = game.player_1
-until game.game_over do
+
+until game.game_over? do
+  game.print_board
   puts "#{player.name}, please enter your play."
-  response = gets.chomp
-  game.check_response(response,player)
-	break if game.game_over
+  computer_reply = game.check_response(gets.chomp,player)
+  if computer_reply
+    puts computer_reply
+    redo
+  end
+	break if game.game_over?
+  player == game.player_1 ? player = game.player_2 : player = game.player_1
 end
 
+puts "game over"
 
-puts "#{@player_1.name.upcase} WINS..." + " #{@player_2.name.upcase} is a LOSER"
-puts "#{@player_2.name.upcase} WINS..." + " #{@player_1.name.upcase} is a LOSER"
-
-puts "It's a draw!"
+# puts "#{@player_1.name.upcase} WINS..." + " #{@player_2.name.upcase} is a LOSER"
+# puts "#{@player_2.name.upcase} WINS..." + " #{@player_1.name.upcase} is a LOSER"
+#
+# puts "It's a draw!"
